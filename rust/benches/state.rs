04003ef3ce7d9db265b5f12becb6aba9bedc7f2b
Mike Kalvas <mkalvas@samcart.com>
2023-08-19 21:53:00 -0400

    tests: reduce setup overhead in benches

--- rust/benches/state.rs
+++ rust/benches/state.rs
@@ -6,9 +6,10 @@ fn create_state(pattern: &str) -> State {
 }
 
 fn step_n(c: &mut Criterion, pattern: &str, n: usize) {
+    let base_state = create_state(pattern);
     c.bench_function(format!("step {n} {pattern}").as_str(), |b| {
         b.iter(|| {
-            let mut state = create_state(pattern);
+            let mut state = base_state.clone();
             for _ in 1..=n {
                 state.step();
             }
